860f6734936d73db1fb0d11b08eddd3053a55d5c
kyle-g-stubblefield <kyle.g.stubblefield@gmail.com>
2026-05-15 18:59:16 +0000

    fixed lindi

--- zones/lindi/zone.py
+++ zones/lindi/zone.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 number = 10
-=======
-number = myNumber
-=======
-number = 10
->>>>>>> 9fe502f57af73db72fddb8938a1e7be67f770817
 name=""
 author=""
->>>>>>> 8d10f38c18685af04beaea170171d355026f39cf
